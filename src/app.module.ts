a5d426131286677a9378923d22adf61949c4fa8c
mschoi9101 <choiminseo@gm.gist.ac.kr>
2024-06-22 05:43:01 +0900

    recruit 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ContentsModule } from './contents/contents.module';
+import { RecruitModule } from './recruit/recruit.module';
 
 @Module({
-  imports: [AuthModule, ContentsModule],
+  imports: [AuthModule, ContentsModule, RecruitModule],
   controllers: [AppController],
   providers: [AppService],
 })
